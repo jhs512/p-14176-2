b9bdc6e23ef6f7207d9dae6d57a2fe4aae876740
jhs512 <jangka512@gmail.com>
2026-06-02 16:41:15 +0900

    ApiV1MemberController 추가 (#4)

---
+++ back/src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
@@ -0,0 +1,9 @@
+package com.back.domain.member.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/members")
+public class ApiV1MemberController {
+}
